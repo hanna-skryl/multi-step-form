48a758907ef28e0c81673a5022ba3172060a3f75
hanna-skryl <hanna.skryl@gmail.com>
2024-04-28 17:27:34 -0400

    Testing things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'multi-step-form',
-    pathMatch: 'full',
-  },
-  {
-    path: 'multi-step-form',
     loadComponent: () => import('./multi-step-form/multi-step-form.component').then(c => c.MultiStepFormComponent),
     title: 'Multi-Step Form',
   },
